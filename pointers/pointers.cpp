ef06737feaa444d2dccfad8a678258c2ade623d5
manueltov <34443692+manueltov@users.noreply.github.com>
2019-02-26 11:13:11 +0000

    Add pointers part

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -3,14 +3,26 @@
 
 using namespace std;
 
+void makemeYoung(int* age){
+    cout << "I used to be " << *age << endl;
+    *age = 21;
+}
+
 int main() {
   int myAge = 23;
   char myGrade = 'F';
+
+  int* agePtr = &myAge;
 
   cout << "Size of int " << sizeof(myAge) << endl;
   cout << "Size of char " << sizeof(myGrade) << endl;
 
   cout << "myAge is locate at " << &myAge << endl;
 
+  cout << "myAge is locate at " << agePtr << endl;
+
+  makemeYoung(&myAge);
+
+  cout << "And now i have: " << *agePtr << endl;
   return 0;
 }
